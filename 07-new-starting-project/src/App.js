d552812e982d222a3dfa6d3c216758961092dd33
abhana <abhana01@gmail.com>
2021-08-25 16:12:36 +0200

    Props Children + Layout

--- 07-new-starting-project/src/App.js
+++ 07-new-starting-project/src/App.js
@@ -1,26 +1,28 @@
 import { Route, Switch } from "react-router-dom";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
+
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavouritesPage from "./pages/Favourites";
 import NewMeetupPage from "./pages/NewMeetup";
 
 function App() {
   return (
-  <div>
-    <MainNavigation />
-    <Switch>
-    {/* <Route path='/' exact={true}> */}
-    <Route path='/' exact>
-      <AllMeetupsPage />
-    </Route>
-    <Route path='/favourites'>
-      <FavouritesPage />
-    </Route>
-    <Route path='/new-meetup'>
-      <NewMeetupPage />
-    </Route>
-    </Switch>
-  </div>
+    <div>
+      <Layout>
+        <Switch>
+          {/* <Route path='/' exact={true}> */}
+          <Route path="/" exact>
+            <AllMeetupsPage />
+          </Route>
+          <Route path="/favourites">
+            <FavouritesPage />
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetupPage />
+          </Route>
+        </Switch>
+      </Layout>
+    </div>
   );
 }
 
