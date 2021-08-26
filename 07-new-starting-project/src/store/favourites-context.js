e95c60255163efb08b921b687d0373ef4b48a2a5
abhana <abhana01@gmail.com>
2021-08-26 16:49:22 +0200

    added React Context + context Provider

---
+++ 07-new-starting-project/src/store/favourites-context.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { createContext } from "react";
+import { useState } from "react";
+
+const FavouritesContext = createContext({
+  favourites: [],
+  totalFavourites: 0,
+  AddFavourite: (favouriteMeetup) => {},
+  removeFavourite: (meetupId) => {},
+  itemIsFavourite: (meetupId) => {},
+});
+
+export function FavouritesContextProvider(props) {
+  const [userFavourites, setuserFavourites] = useState([]);
+
+  function addFavouriteHandler(favouriteMeetup) {
+    setuserFavourites((prevUserFavourites) => {
+      return prevUserFavourites.concat(favouriteMeetup);
+    });
+  }
+
+  function removeFavouriteHandler(meetupId) {
+    setuserFavourites((prevUserFavourites) => {
+      return prevUserFavourites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+
+  function itemIsFavouriteHandler(meetupId) {
+    return userFavourites.some((meetup) => meetup.id === meetupId);
+  }
+
+  const context = {
+    favourites: userFavourites,
+    totalFavourites: userFavourites.length,
+    AddFavourite: addFavouriteHandler,
+    removeFavourite: removeFavouriteHandler,
+    itemIsFavourite: itemIsFavouriteHandler,
+  };
+
+  return (
+    <FavouritesContext.Provider value={context}>
+      {props.children}
+    </FavouritesContext.Provider>
+  );
+}
+
+export default FavouritesContext;
