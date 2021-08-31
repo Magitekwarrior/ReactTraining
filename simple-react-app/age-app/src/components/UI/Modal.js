99e9e8bb12d129fc200d5aa53da99b8ee7993702
abhana <abhana01@gmail.com>
2021-08-31 15:26:27 +0200

    example of React Portal

--- simple-react-app/age-app/src/components/UI/Modal.js
+++ simple-react-app/age-app/src/components/UI/Modal.js
@@ -1,24 +1,51 @@
 import React from "react";
 import classes from "./Modal.module.css";
+import ReactDOM from "react-dom";
 
-const Modal = (props) => {
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
+};
+
+const ModalOverlay = (props) => {
   return (
-    <div className={classes.backdrop}>
-      <div className={classes.modal}>
-        <div className={classes.modalcontent}>
-          <div className={classes.modalheader}>
-            <span className={classes.close} onClick={props.onConfirm}>&times;</span>
-            <h2>{props.title}</h2>
-          </div>
-          <div className={classes.modalbody}>
-            <p>{props.message}</p>
-          </div>
-          <div className={classes.modalfooter}>
-            <button className={classes.modalbtn} onClick={props.onConfirm}>Okay</button>
-          </div>
+    <div className={classes.modal}>
+      <div className={classes.modalcontent}>
+        <div className={classes.modalheader}>
+          <span className={classes.close} onClick={props.onConfirm}>
+            &times;
+          </span>
+          <h2>{props.title}</h2>
+        </div>
+        <div className={classes.modalbody}>
+          <p>{props.message}</p>
+        </div>
+        <div className={classes.modalfooter}>
+          <button className={classes.modalbtn} onClick={props.onConfirm}>
+            Okay
+          </button>
         </div>
       </div>
     </div>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm}></Backdrop>,
+        document.getElementById("backdrop-root")
+      )}
+
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          onConfirm={props.onConfirm}
+          title={props.title}
+          message={props.message}
+        ></ModalOverlay>,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 
